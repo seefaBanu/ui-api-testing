5f9ffd75d12f5a3c4da95461e17b1eb79a8f4962
NuskaFayik <mffnuska@gmail.com>
2024-12-26 17:16:11 +0530

    create book API test case

--- api-testing/src/test/java/stepdefinitions/CreateBookApiStepDefinitions.java
+++ api-testing/src/test/java/stepdefinitions/CreateBookApiStepDefinitions.java
@@ -1,12 +1,11 @@
 package stepdefinitions;
 
 import base.TestBase;
-import io.cucumber.java.en.And;
-import io.restassured.response.Response;
 import io.cucumber.java.en.Given;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
 import io.cucumber.java.en.When;
 import io.cucumber.java.en.Then;
-import io.restassured.specification.RequestSpecification;
 import org.testng.Assert;
 
 import java.util.Map;
@@ -14,15 +13,14 @@ import java.util.Map;
 public class CreateBookApiStepDefinitions extends TestBase {
     private Response response;
 
+
     @When("user sends a POST request to {string} with the following data:")
     public void user_sends_post_request_with_data(String endpoint,Map<String, String> data) {
-//        String data = String.format("{ \"title\": \"%s\", \"author\": \"%s\" }", title, author);
         response = userRequest.body(data).post(endpoint);
     }
 
     @When("user sends a POST request to {string} with invalid data:")
     public void user_sends_post_request_with_invalid_data(String endpoint, Map<String, String> data ) {
-//        String data = String.format("{ \"title\": \"%s\", \"author\": \"%s\" }", title, author);
         response = userRequest.body(data).post(endpoint);
     }
 
@@ -40,6 +38,35 @@ public class CreateBookApiStepDefinitions extends TestBase {
     public void response_should_contain_book_title_details() {
         Assert.assertTrue(response.getBody().asString().contains("title"), "Book details are missing in response");
     }
+
+
+    @When("un authenticated POST request to {string} with the following data:")
+    public void user_sends_post_request_without_authentication(String endpoint, Map<String, String> data) {
+        response = unAuthenticatedRequest.body(data).post(endpoint); // Sending the request without authentication
+    }
+
+
+
+//    @When("user sends a POST request to {string} with the following data:")
+//    public void user_sends_post_request_with_empty_fields(String endpoint, Map<String, String> data) {
+//        response = userRequest.body(data).post(endpoint); // Sending the request with empty fields
+//    }
+//
+//
+//
+//    @When("user sends a POST request to {string} with the following data:")
+//    public void user_sends_post_request_with_duplicate_title(String endpoint, Map<String, String> data) {
+//        response = userRequest.body(data).post(endpoint); // Sending the request with duplicate title
+//    }
+//
+//
+//
+//
+//    @When("user sends a POST request to {string} with the following data:")
+//    public void user_sends_post_request_with_invalid_author_type(String endpoint, Map<String, String> data) {
+//        response = userRequest.body(data).post(endpoint); // Sending the request with invalid author type
+//    }
+
 
 
 }
