58537074fe48276aee9a52b44777339b9f80dc3d
seefaBanu <banuags.20@uom.lk>
2024-12-26 23:47:12 +0530

    add allure reporting and create book api testing

--- api-testing/src/test/java/stepdefinitions/CreateBookApiStepDefinitions.java
+++ api-testing/src/test/java/stepdefinitions/CreateBookApiStepDefinitions.java
@@ -1,6 +1,8 @@
 package stepdefinitions;
 
 import base.TestBase;
+import io.cucumber.java.After;
+import io.cucumber.java.BeforeAll;
 import io.restassured.response.Response;
 import io.cucumber.java.en.When;
 import io.cucumber.java.en.Then;
@@ -9,31 +11,35 @@ import org.testng.Assert;
 import java.util.Map;
 
 public class CreateBookApiStepDefinitions extends TestBase {
-    private Response response;
+    public Response response;
 
-    @When("user sends a POST request to {string} with the following data:")
-    public void user_sends_post_request_with_data(String endpoint,Map<String, String> data) {
+    @When("{string} sends a POST request to {string} with valid data:")
+    public void user_sends_post_request_with_valid_data(String user, String endpoint, Map<String, String> data) {
+        if (user.equals("user"))
+            response = userRequest.body(data).post(endpoint);
+        else if (user.equals("admin")){
+            response = adminRequest.body(data).post(endpoint);
+        }
+    }
+
+    @When("user sends a POST request to {string} without mandatory field")
+    public void user_sends_post_request_without_mandatory_field(String endpoint, Map<String, String> data) {
         response = userRequest.body(data).post(endpoint);
     }
 
-    @When("user sends a POST request to {string} with invalid data:")
-    public void user_sends_post_request_with_invalid_data(String endpoint, Map<String, String> data ) {
-        response = userRequest.body(data).post(endpoint);
-    }
-
-    @Then("the response for creating book api status code should be {int}")
-    public void the_response_for_creating_book_api_status_code_should_be(int expectedStatusCode) {
+    @Then("the response status code for Create Book API should be {int}")
+    public void response_status_code_for_create_api_should_be(int expectedStatusCode) {
         Assert.assertEquals(response.getStatusCode(), expectedStatusCode, "Unexpected status code!");
     }
 
-    @Then("the response should contain book author details")
-    public void response_should_contain_book_author_details() {
-        Assert.assertTrue(response.getBody().asString().contains("author"), "Book details are missing in response");
+    @Then("the response body for Create Book API should be {string}")
+    public void response_body_for_create_api_should_be(int message) {
+        Assert.assertEquals(response.getBody().asString(), message, "Unexpected status code!");
     }
 
-    @Then("the response should contain book title details")
-    public void response_should_contain_book_title_details() {
-        Assert.assertTrue(response.getBody().asString().contains("title"), "Book details are missing in response");
+    @After
+    public void tearDown() {
+        response = null;
     }
 
 }
--- api-testing/src/test/resources/features/create_book_api.feature
+++ api-testing/src/test/resources/features/create_book_api.feature
@@ -1,18 +1,32 @@
-
 Feature: Create Book API Testing
 
-  Scenario: User tries to create a book
+  Scenario: User creates a book with valid data
     Given user is authenticated
-    When user sends a POST request to "/api/books" with the following data:
+    When "user" sends a POST request to "/api/books" with valid data:
       | title   | Mocking Bird |
-      | author | Lovers lee|
-    Then the response for creating book api status code should be 201
+      | author | Harpers lee|
+    Then the response status code for Create Book API should be 201
 
-  Scenario: User tries to create a book with invalid data
+  Scenario: User creates a book without title
     Given user is authenticated
-    When user sends a POST request to "/api/books" with invalid data:
-      | title   |  Mocking pet |
-      | author | Author4|
-    Then the response for creating book api status code should be 400
+    When user sends a POST request to "/api/books" without mandatory field
+      | author | Rawling|
+    Then the response status code for Create Book API should be 400
+    And the response body for Create Book API should be "Mandatory parameters should not be null"
+
+  Scenario: User creates a book without author
+    Given user is authenticated
+    When user sends a POST request to "/api/books" without mandatory field
+      | title | Winged Dreams|
+    Then the response status code for Create Book API should be 400
+    And the response body for Create Book API should be "Mandatory parameters should not be null"
+
+  Scenario: Admin creates a book without author
+    Given admin is authenticated
+    When "admin" sends a POST request to "/api/books" with valid data:
+      | title   | The Kite Runner |
+      | author | Khalid |
+    Then the response status code for Create Book API should be 201
+
 
     
