85277484f7c821abe43a1daee6e6ad5f6ee78f17
ulfathAkf <114693849+ulfathAkf@users.noreply.github.com>
2024-12-26 21:38:20 +0530

    Update api test cases

--- api-testing/src/test/java/stepdefinitions/UpdateBookApiStepDefinitions.java
+++ api-testing/src/test/java/stepdefinitions/UpdateBookApiStepDefinitions.java
@@ -1,12 +1,44 @@
 package stepdefinitions;
 
 import base.TestBase;
-import io.restassured.response.Response;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+import io.cucumber.java.en.Then;
+import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
+import org.testng.Assert;
 
+
+import java.util.Map;
 public class UpdateBookApiStepDefinitions extends TestBase {
     private Response response;
 
 
+    @When("{string} sends a PUT request to {string} with the following valid data:")
+    public void admin_sends_put_request_with_valid_data(String user, String endpoint, Map<String, String> data) {
+        // Replace the placeholder {id} with the actual book id
+        if (user.equals("user")) {
+            response = TestBase.userRequest
+                    .body(data)
+                    .put(endpoint);
+        } else if (user.equals("admin")) {
+            response = TestBase.adminRequest
+                    .body(data)
+                    .put(endpoint);
+
+        }
+
+    }
+
+    @Then("the response status code for Updating Book API should be {int}")
+    public void the_response_status_code_should_be(int expectedStatusCode) {
+        Assert.assertEquals(response.getStatusCode(), expectedStatusCode, "Unexpected status code!");
+    }
+
+    @And("the response body for Update Book API should be {string}")
+    public void the_response_body_for_update_book_api_should_be(String expectedMessage) {
+        String responseBody = response.getBody().asString();
+        Assert.assertTrue(responseBody.contains(expectedMessage), "Response body does not contain expected message: " + expectedMessage);
+    }
 }
--- api-testing/src/test/resources/features/update_book_api.feature
+++ api-testing/src/test/resources/features/update_book_api.feature
@@ -1,1 +1,37 @@
 Feature: Update Book API Testing
+
+  Scenario: Admin updates the book with missing mandatory fields
+    Given admin is authenticated
+    When "admin" sends a PUT request to "/api/books/1" with the following valid data:
+      | id  | 1 |
+      | title    |      |
+      |  author | aysha  |
+    Then the response status code for Updating Book API should be 400
+
+
+  Scenario: Admin updates the details of a book
+    Given admin is authenticated
+    When "admin" sends a PUT request to "/api/books/1" with the following valid data:
+      | id  | 1 |
+      | title   | heart break  |
+      |  author | Banu  |
+    Then the response status code for Updating Book API should be 200
+
+
+  Scenario: Admin updates the book with non-existing id
+    Given admin is authenticated
+    When "admin" sends a PUT request to "/api/books/999" with the following valid data:
+      | id  | 999 |
+      | title   | roses  |
+      |  author | ulfath  |
+    Then the response status code for Updating Book API should be 404
+    And the response body for Update Book API should be "Book not found"
+
+  Scenario: User updates the details of a book
+    Given user is authenticated
+    When "user" sends a PUT request to "/api/books/1" with the following valid data:
+      | id  | 1 |
+      | title   | mountains echoed  |
+      |  author | kalid hosainee   |
+    Then the response status code for Updating Book API should be 403
+    And the response body for Update Book API should be "User is not permitted"
