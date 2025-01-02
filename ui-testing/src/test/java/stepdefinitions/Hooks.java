dcab320a16a79937065da8b73805265afaa36921
israthnr <israthnr.20@uom.lk>
2025-01-03 04:40:12 +0530

    Delete Created Address After All

--- ui-testing/src/test/java/stepdefinitions/Hooks.java
+++ ui-testing/src/test/java/stepdefinitions/Hooks.java
@@ -1,7 +1,12 @@
 package stepdefinitions;
 
+import com.itqa.pages.AddressBookPage;
+import com.itqa.pages.CartPage;
+import com.itqa.pages.HomePage;
+import com.itqa.pages.MyAccountPage;
 import com.itqa.utils.DriverFactory;
 import io.cucumber.java.After;
+import io.cucumber.java.AfterAll;
 import io.cucumber.java.Before;
 import org.openqa.selenium.WebDriver;
 
@@ -18,5 +23,42 @@ public class Hooks {
     public void tearDown() {
         // Quit the WebDriver after each scenario
         DriverFactory.quitDriver();
+    }
+
+    @AfterAll
+    public static void cleanup() {
+        System.out.println("Cleaning up test data...");
+
+        // Remove all products from the cart
+//        try {
+//            CartPage cartPage = new CartPage(DriverFactory.getDriver());
+//            cartPage.goToCartPage();
+//            while (cartPage.getCartItemCount() > 0) {
+//                cartPage.removeItemFromCart();
+//                cartPage.deleteConfirmationPopup();
+//            }
+//            System.out.println("All products removed from the cart.");
+//        } catch (Exception e) {
+//            System.err.println("Error removing products from the cart: " + e.getMessage());
+//        }
+
+        // Remove all addresses
+        try {
+            LoginSteps loginSteps = new LoginSteps();
+            loginSteps.userIsOnTheLoginPage();
+            loginSteps.userLogsInWithUsernameAndPassword();
+            HomePage homePage = new HomePage(DriverFactory.getDriver());
+            homePage.clickTheToggle();
+            MyAccountPage myAccountPage = new MyAccountPage(DriverFactory.getDriver());
+            AddressBookPage addressBookPage = new AddressBookPage(DriverFactory.getDriver());
+
+            myAccountPage.goToMyAccountPage();
+            myAccountPage.clickAddressBookLink();
+
+            addressBookPage.removeAllAddresses();
+            System.out.println("All addresses removed.");
+        } catch (Exception e) {
+            System.err.println("Error removing addresses: " + e.getMessage());
+        }
     }
 }
