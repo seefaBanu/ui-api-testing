7a179f2554e68764be3a1603de3397d688eaf842
seefaBanu <banuags.20@uom.lk>
2025-01-02 21:10:43 +0530

    Add login as precondition to Add to Cart test

--- ui-testing/src/test/java/stepdefinitions/LoginSteps.java
+++ ui-testing/src/test/java/stepdefinitions/LoginSteps.java
@@ -1,70 +1,29 @@
-//package stepdefinitions;
-//
-//import io.cucumber.java.After;
-//import io.cucumber.java.Before;
-//import io.cucumber.java.en.Given;
-//import io.cucumber.java.en.Then;
-//import io.cucumber.java.en.When;
-//import org.openqa.selenium.By;
-//import org.openqa.selenium.WebDriver;
-//import org.openqa.selenium.chrome.ChromeDriver;
-//import org.openqa.selenium.chrome.ChromeOptions;
-//import org.testng.Assert;
-//import java.time.Duration;
-//
-//public class LoginSteps {
-//    private static WebDriver driver;
-//    public final static int TIMEOUT = 10;
-//
-//    @Before
-//    public void setUp() {
-//        ChromeOptions options = new ChromeOptions();
-//        options.addArguments("--start-maximized");
-//        driver = new ChromeDriver(options);
-//        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(TIMEOUT));
-//    }
-//
-//    @Given("User is on HRMLogin page {string}")
-//    public void loginTest(String url) {
-//
-//        driver.get(url);
-//
-//    }
-//
-//    @When("User enters username as {string} and password as {string}")
-//    public void goToHomePage(String userName, String passWord) {
-//
-//        // login to application
-//        driver.findElement(By.name("login[username]")).sendKeys(userName);
-//        driver.findElement(By.xpath("//*[@id=\"pass\"]")).sendKeys(passWord);
-//        driver.findElement(By.xpath("//*[@id=\"send2\"]")).submit();
-//
-//    }
-//
-//    @Then("User should be able to login sucessfully and new page open")
-//    public void userShouldBeAbleToLoginSucessfullyAndNewPageOpen() {
-//
-//        String homePageHeading = driver.findElement(By.xpath("//*[@id=\"maincontent\"]/div[1]/h1/span")).getText();
-//
-//        //Verify new page - HomePage
-//        Assert.assertEquals(homePageHeading, "Home Page");
-//    }
-//
-//    @Then("User should be able to see error message {string}")
-//    public void verifyErrorMessage(String expectedErrorMessage) {
-//
-//        String actualErrorMessage = driver.findElement(By.xpath("//*[@id=\"maincontent\"]/div[2]/div[2]/div/div/div")).getText();
-//
-//        // Verify Error Message
-//        Assert.assertEquals(actualErrorMessage, expectedErrorMessage);
-//
-//        driver.quit();
-//
-//    }
-//
-//    @After
-//    public void teardown() {
-//        driver.quit();
-//    }
-//
-//}
+package stepdefinitions;
+
+import com.itqa.pages.HomePage;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+import org.openqa.selenium.By;
+
+public class LoginSteps extends BaseSteps {
+    private HomePage homePage;
+
+    public LoginSteps() {
+        super();
+        homePage = new HomePage(driver);
+    }
+
+    @Given("User is on the login page")
+    public void userIsOnTheLoginPage() {
+        homePage.navigateToHomePage("https://magento.softwaretestingboard.com/");
+        homePage.clickSignIn();
+    }
+
+    @When("User logs in with username and password")
+    public void userLogsInWithUsernameAndPassword() {
+        driver.findElement(By.name("login[username]")).sendKeys("testuser123test@gmail.com");
+        driver.findElement(By.id("pass")).sendKeys("Test@123");
+        driver.findElement(By.id("send2")).click();
+    }
+
+}
--- ui-testing/src/test/resources/features/remove_from_cart.feature
+++ ui-testing/src/test/resources/features/remove_from_cart.feature
@@ -4,7 +4,8 @@ Feature: remove product from cart
   So that I can update my selection
 
   Scenario: Remove a product from the cart
-    Given I am on the homepage
+    Given User is on the login page
+    And User logs in with username and password
     When I go to the cart page
     And I click Remove for the product
     Then I click OK on confirmation popup
